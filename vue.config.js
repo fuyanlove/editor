3d5f288363950a0e0186f372e79cf994ee823b19
浮烟 <rx6@qq.com>
2023-02-09 01:16:24 +0800

    Initial commit

---
+++ vue.config.js
@@ -0,0 +1,79 @@
+/*
+ * @Author: iRuxu
+ * @Date: 2022-07-04 10:42:02
+ * @LastEditTime: 2022-07-08 19:27:07
+ * @Description:config
+ */
+const path = require("path");
+const pkg = require("./package.json");
+const project = require("./project.json");
+module.exports = {
+    //❤️ Multiple pages ~
+    // pages: {
+    //     index: {
+    //         title: "Home",
+    //         entry: "src/main.js",
+    //         template: "public/index.html",
+    //         filename: "index.html",
+    //     },
+    //     $project: {
+    //         title: "Project",
+    //         entry: "src/core/$project/index.js",
+    //         template: "public/$project/index.html",
+    //         filename: "$project/index.html",
+    //     },
+    // },
+
+    //⚛️ Proxy ~
+    devServer: {
+        proxy: {
+            // "/api/cms": {
+            //     target: process.env["DEV_SERVER"] == "true" ? "http://localhost:5120" : "https://xxx.deepberry.cn",
+            // },
+        },
+    },
+
+    //❤️ define path for static files ~
+    publicPath: process.env.NODE_ENV === "development" ? "/" : `${process.env.STATIC_PATH}${pkg.name}/`,
+
+    //❤️ Webpack configuration
+    chainWebpack: (config) => {
+        //💘 html-webpack-plugin ~
+        // Multiple pages disable the block below
+        config.plugin("html").tap((args) => {
+            args[0].meta = {
+                //------设置SEO信息
+                Keywords: project.keys,
+                Description: project.desc,
+            };
+            args[0].title = project.title; //------自动添加标题后缀
+            return args;
+        });
+
+        //💝 in-line small imgs ~
+        config.module.rule("images").set("parser", {
+            dataUrlCondition: {
+                maxSize: 4 * 1024, // 4KiB
+            },
+        });
+
+        //💝 in-line svg imgs ~
+        config.module.rule("vue").use("vue-svg-inline-loader").loader("vue-svg-inline-loader");
+
+        //💖 import common less var * mixin ~
+        const types = ["vue-modules", "vue", "normal-modules", "normal"];
+        types.forEach((type) => addStyleResource(config.module.rule("less").oneOf(type)));
+    },
+};
+
+function addStyleResource(rule) {
+    var preload_styles = [];
+    preload_styles.push(
+        path.resolve(__dirname, "./node_modules/csslab/base.less"),
+        path.resolve(__dirname, "./node_modules/@deepberry/common/css/global.less"),
+        path.resolve(__dirname, "./src/assets/css/var.less")
+    );
+    rule.use("style-resource").loader("style-resources-loader").options({
+        patterns: preload_styles,
+    });
+}
