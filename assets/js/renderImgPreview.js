d7083be1b397aaf2fd701917ac02d8f3ba927002
Kaviilee <jiaweilee95@126.com>
2023-03-09 16:30:36 +0800

    feat: udpate viewerjs

--- assets/js/renderImgPreview.js
+++ assets/js/renderImgPreview.js
@@ -1,11 +1,17 @@
+import $ from "jquery";
 import "viewerjs/dist/viewer.css";
 import Viewer from "viewerjs";
 
-function renderImgPreview(vm, selector = ".c-article") {
-    // 之后可能会有排除的情况
-    new Viewer(document.querySelector(selector), {
-        toolbar: false,
-        navbar: false,
+function renderImgPreview(selector = ".c-article img") {
+    // 获取src不为空的图片
+    let imgs = $(selector).filter(function () {
+        return $(this).attr("src") != "";
+    });
+    imgs.each((i, ele) => {
+        new Viewer(ele, {
+            toolbar: false,
+            navbar: false,
+        });
     });
 }
 
